c35e8b2981e91af244a2fc1069d8dd4c4d3b0dda
ayersm <matt_ayers@hotmail.co.uk>
2018-08-04 16:55:23 +0100

    Session 3
    Check My Work

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,7 +10,7 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
-
+        
         public override char GetLetterGrade(double averageGrade)
         {
             if(Students.Count < 5)            
@@ -29,6 +29,26 @@ namespace GradeBook.GradeBooks
                 return 'D';
             else
                 return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStatistics();            
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+                base.CalculateStudentStatistics(name);
         }
     }
 }
